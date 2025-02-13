18a0c34ed79a26524d6b75b388c109e5c1996572
IT13A_MARGATE <margatelovegrace09@gmail.com>
2025-02-13 11:29:54 +0800

    added arithmetic

---
+++ Prelim/Lab_act1_arithmetic.java
@@ -0,0 +1,20 @@
+package Prelim;
+
+
+public class Lab_act1_arithmetic {
+    public static void main (String [] args){
+  
+        int firstNumber = 10;
+        int secondNumber = 4;
+        int thirdNumber = 6;
+
+        System.out.println(firstNumber + " + " + secondNumber + " + " + thirdNumber + " = " + (firstNumber + secondNumber + thirdNumber));
+        System.out.println("(" + firstNumber + " * " + secondNumber + ") % " + thirdNumber + " = " + ((firstNumber * secondNumber) % thirdNumber));
+        System.out.println(firstNumber + " * " + secondNumber + " + " + thirdNumber + " = " + (firstNumber * secondNumber + thirdNumber));
+        System.out.println(firstNumber + " + " + thirdNumber + " = " + (firstNumber + thirdNumber));
+        
+    }
+}
+
+   
+
